36161f29ee26f7d44fe1cc362400b12b57676142
giriyg <ygg3y8@missouri.edu>
2024-02-21 13:28:30 -0600

    format doc

--- src/main.ts
+++ src/main.ts
@@ -10,26 +10,28 @@ class Grid {
   }
   /**
    * Helper function to initialize a new 2D array
-   * @returns 
+   * @returns
    */
   createArr() {
-    const arr = new Array(this.res).fill(null).map(() => new Array(this.res).fill(0));
+    const arr = new Array(this.res)
+      .fill(null)
+      .map(() => new Array(this.res).fill(0));
     return arr;
   }
   /**
    * Get the value at a given position
-   * @param x 
-   * @param y 
-   * @returns 
+   * @param x
+   * @param y
+   * @returns
    */
   at(x: number = 0, y: number = 0) {
     return this.arr[x][y];
   }
   /**
    * Set the value at a given position
-   * @param x 
-   * @param y 
-   * @param val 
+   * @param x
+   * @param y
+   * @param val
    */
   set(x: number = 0, y: number = 0, val: number = 0) {
     if (x >= 0 && x < this.arr.length && y >= 0 && y < this.arr[0].length) {
@@ -43,17 +45,18 @@ class Grid {
     const newArr = this.createArr();
     for (let i = 0; i < this.res; i++) {
       for (let j = 0; j < this.res; j++) {
-        let val = this.arr[i][j]/9;
-        for(let x = -1; x <= 1; x++){
-          for(let y = -1; y <= 1; y++){
+        let val = this.arr[i][j] / 9;
+        for (let x = -1; x <= 1; x++) {
+          for (let y = -1; y <= 1; y++) {
             const i_ = i + x;
             const j_ = j + y;
-            if(i_ >= 0 && j_ >= 0 && i_ < this.res && j_ < this.res){
+            if (i_ >= 0 && j_ >= 0 && i_ < this.res && j_ < this.res) {
               newArr[i_][j_] += val;
-              if(newArr[i_][j_]>1){
+              if (newArr[i_][j_] > 1) {
                 newArr[i_][j_] = 1;
-              }if(newArr[i_][j_]<-1){
-                newArr[i_][j_] -1;
+              }
+              if (newArr[i_][j_] < -1) {
+                newArr[i_][j_] - 1;
               }
             }
           }
@@ -64,9 +67,8 @@ class Grid {
   }
 }
 
-
 let timer = 0;
-let w = canvas.width/40;
+let w = canvas.width / 40;
 const grid = new Grid();
 const mouse = {
   down: false,
@@ -79,33 +81,32 @@ canvas.addEventListener("mouseup", (e) => {
 });
 canvas.addEventListener("mousemove", (e) => {
   if (mouse.down) {
-    const x = Math.floor(e.x/w);
-    const y = Math.floor(e.y/w);
+    const x = Math.floor(e.x / w);
+    const y = Math.floor(e.y / w);
     grid.set(x, y, 5);
   }
 });
 
-function animate(){
+function animate() {
   grid.update();
-  c.fillStyle = "rgba(0,0,0,0.05)"
+  c.fillStyle = "rgba(0,0,0,0.05)";
   c.fillRect(0, 0, canvas.width, canvas.height);
   c.beginPath();
   for (let i = 0; i < grid.res; i++) {
     for (let j = 0; j < grid.res; j++) {
       const val = grid.arr[i][j];
-      let r = val>0? 255:0;
-      let g = Math.floor(Math.abs(val) * 255)
-      let b = val<=0? 255:0;
+      let r = val > 0 ? 255 : 0;
+      let g = Math.floor(Math.abs(val) * 255);
+      let b = val <= 0 ? 255 : 0;
       c.fillStyle = `rgba(${r},${g},${b},${Math.abs(val)})`;
       c.fillRect(i * w, j * w, 10, 10);
     }
   }
 
-  timer+=0.05;
-  grid.arr[30][20] = 5*Math.sin(timer);
-  grid.arr[10][20] = 5*Math.cos(timer);
+  timer += 0.05;
+  grid.arr[30][20] = 5 * Math.sin(timer);
+  grid.arr[10][20] = 5 * Math.cos(timer);
   c.closePath();
   requestAnimationFrame(animate);
 }
-  animate();
-
+animate();
