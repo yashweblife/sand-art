66c901393f6d213e6d5145ae65b1e5aa54d8b6ed
yashweblife <yashweblife20@gmail.com>
2025-07-07 15:27:59 -0500

    try size changes

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,7 @@
 import Grid from "./Grid";
 const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
-canvas.height = 500;
-canvas.width = 500;
+canvas.height = 700;
+canvas.width = 700;
 const c = canvas.getContext("2d") as CanvasRenderingContext2D;
 const mouse = {
   down: false,
