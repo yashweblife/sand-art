b475984d70d8914b3141f6e74e86fecefbd02b33
yashweblife <yashweblife20@gmail.com>
2025-07-07 15:10:35 -0500

    add more consts to export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,10 @@
 import Grid from "./Grid";
-export { Grid };
+const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
+canvas.height = 500;
+canvas.width = 500;
+const c = canvas.getContext("2d") as CanvasRenderingContext2D;
+const mouse = {
+  down: false,
+};
+export { c, canvas, Grid, mouse };
+
